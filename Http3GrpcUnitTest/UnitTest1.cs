2700f938b2bf8f3e14c11caa766490334b2672d4
gradx <gradx7@gmail.com>
2022-12-30 18:05:37 -0800

    Added gRPC + Http3 to an Http3 only port which fails on the first  attempt

--- Http3GrpcUnitTest/UnitTest1.cs
+++ Http3GrpcUnitTest/UnitTest1.cs
@@ -3,6 +3,7 @@ using GrpcService1;
 using System;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Channels;
 
 namespace Http3GrpcUnitTest
 {
@@ -10,7 +11,10 @@ namespace Http3GrpcUnitTest
     {
         public const string c_SELF_SIGNED_URL = "https://mydomain.com:5001";
         public const string c_CHAINED_URL = "https://chained.mydomain.com:5002";
+        public const string c_HTTP3_SELF_SIGNED_URL = "https://mydomain.com:5003";
+        public const string c_HTTP3_CHAINED_URL = "https://chained.mydomain.com:5004";
 
+        #region Self Signed Tests
         [Fact]
         public async Task HttpVersion20SelfSignedTest()
         {
@@ -39,6 +43,28 @@ namespace Http3GrpcUnitTest
         }
 
         [Fact]
+        public async Task HttpVersion30OnlySelfSignedTest()
+        {
+            var client = UnitTestHelpers.CreateHttpClient(HttpVersion.Version30, "ca.pfx");
+            var result = await UnitTestHelpers.Ping(client, c_HTTP3_SELF_SIGNED_URL);
+            Assert.True(result == "Pong");
+        }
+
+        [Fact]
+        public async Task HttpVersion30OnlyGrpcTest()
+        {
+            var channel = GrpcChannel.ForAddress(c_HTTP3_SELF_SIGNED_URL,
+                new GrpcChannelOptions() { HttpClient = UnitTestHelpers.CreateHttpClient(HttpVersion.Version30, "ca.pfx") });
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+            Assert.True(response.Message == "Hello World");
+        }
+
+        #endregion
+
+        #region Chained Tests
+
+        [Fact]
         public async Task HttpVersion20ChainedTest()
         {
             var client = UnitTestHelpers.CreateHttpClient(HttpVersion.Version20, "client.pfx");
@@ -64,5 +90,26 @@ namespace Http3GrpcUnitTest
             var response = await client.SayHelloAsync(new HelloRequest { Name = "World" });
             Assert.True(response.Message == "Hello World");
         }
+
+        [Fact]
+        public async Task HttpVersion30OnlyChainedTest()
+        {
+            var client = UnitTestHelpers.CreateHttpClient(HttpVersion.Version30, "client.pfx");
+            var result = await UnitTestHelpers.Ping(client, c_HTTP3_CHAINED_URL);
+            Assert.True(result == "Pong");
+        }
+
+        [Fact]
+        public async Task GrpcHttpVersion30OnlyChainedTest()
+        {
+
+            var channel = GrpcChannel.ForAddress(c_HTTP3_CHAINED_URL,
+                new GrpcChannelOptions() { HttpClient = UnitTestHelpers.CreateHttpClient(HttpVersion.Version30, "client.pfx") });
+            var client = new Greeter.GreeterClient(channel);
+            var response = await client.SayHelloAsync(new HelloRequest { Name = "World" });
+            Assert.True(response.Message == "Hello World");
+        }
+
+        #endregion
     }
 }
