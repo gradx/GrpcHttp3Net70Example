eed4bead3821c6920d5fa09db0893bb596ddbf1e
gradx <gradx7@gmail.com>
2022-12-31 21:34:54 -0800

    Simply commenting structure

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -18,15 +18,18 @@ builder.Services.Configure<KestrelServerOptions>(options =>
     options.ConfigureHttpsDefaults(options =>
     {        
         options.ClientCertificateMode = ClientCertificateMode.RequireCertificate;
-        //options.ServerCertificateSelector = (context, subjectName) =>
-        //{
-            
-        //    Console.WriteLine("ServerCertificateSelector:" + subjectName);
-        //    if (subjectName == "mydomain.com")
-        //        return new X509Certificate2("ca.pfx");
-        //    else 
-        //        return new X509Certificate2("badca.pfx");
-        //};
+
+        /*
+        options.ServerCertificateSelector = (context, subjectName) =>
+        {
+
+            Console.WriteLine("ServerCertificateSelector:" + subjectName);
+            if (subjectName == "mydomain.com")
+                return new X509Certificate2("ca.pfx");
+            else
+                return new X509Certificate2("badca.pfx");
+        };
+        */
 
         options.ClientCertificateValidation = (cert, chain, policyErrors) =>
         {
@@ -51,39 +54,41 @@ builder.Services.AddAuthentication(CertificateAuthenticationDefaults.Authenticat
     {
         options.AllowedCertificateTypes = CertificateTypes.All;
         options.RevocationMode = X509RevocationMode.NoCheck;
-        //options.ValidateCertificateUse = false;
-        //options.Events = new CertificateAuthenticationEvents
-        //{
+        /*
+        options.ValidateCertificateUse = false;
+        options.Events = new CertificateAuthenticationEvents
+        {
 
-        //    OnCertificateValidated = context =>
-        //    {
-        //        Debug.WriteLine("Help");
-        //        Console.WriteLine("Help");
-        //        var claims = new[]
-        //        {
-        //            new Claim(
-        //                ClaimTypes.NameIdentifier,
-        //                context.ClientCertificate.Subject,
-        //                ClaimValueTypes.String, context.Options.ClaimsIssuer),
-        //            new Claim(
-        //                ClaimTypes.Name,
-        //                context.ClientCertificate.Subject,
-        //                ClaimValueTypes.String, context.Options.ClaimsIssuer)
-        //        };
+            OnCertificateValidated = context =>
+            {
+                Debug.WriteLine("Help");
+                Console.WriteLine("Help");
+                var claims = new[]
+                {
+                    new Claim(
+                        ClaimTypes.NameIdentifier,
+                        context.ClientCertificate.Subject,
+                        ClaimValueTypes.String, context.Options.ClaimsIssuer),
+                    new Claim(
+                        ClaimTypes.Name,
+                        context.ClientCertificate.Subject,
+                        ClaimValueTypes.String, context.Options.ClaimsIssuer)
+                };
 
-        //        context.Principal = new ClaimsPrincipal(
-        //            new ClaimsIdentity(claims, context.Scheme.Name));
-        //        context.Success();
+                context.Principal = new ClaimsPrincipal(
+                    new ClaimsIdentity(claims, context.Scheme.Name));
+                context.Success();
 
-        //        return Task.CompletedTask;
-        //    },
-        //    OnAuthenticationFailed = context =>
-        //    {
-        //        int test = Convert.ToInt32("test");
-        //        context.Fail($"Invalid certificate");
-        //        return Task.CompletedTask;
-        //    }
-        //};
+                return Task.CompletedTask;
+            },
+            OnAuthenticationFailed = context =>
+            {
+                int test = Convert.ToInt32("test");
+                context.Fail($"Invalid certificate");
+                return Task.CompletedTask;
+            }
+        };
+        */
     });
 
 builder.Services.AddGrpc();
