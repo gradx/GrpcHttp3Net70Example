922934162c43072e1c8a0a456b5d97e79bdeb496
gradx <gradx7@gmail.com>
2023-01-13 01:42:23 -0800

    moved Envoy to parent dir to access certs for Docker

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 sh SSL/certs.sh int.mydomain.com
 
-docker build -t grpchttp3 -f Dockerfile .
-docker build -t envoy -f Envoy/Dockerfile Envoy
-docker run envoy &
-docker run grpchttp3
+docker build -t grpchttp3 -f grpc.Dockerfile .
+docker build -t envoy -f envoy.Dockerfile .
+#docker run envoy &
+#docker run grpchttp3
