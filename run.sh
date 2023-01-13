70907f1a41f34eccac72d16102ac924209690101
gradx <gradx@users.noreply.github.com>
2023-01-13 01:48:49 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ sh SSL/certs.sh int.mydomain.com
 
 docker build -t grpchttp3 -f grpc.Dockerfile .
 docker build -t envoy -f envoy.Dockerfile .
-#docker run envoy &
-#docker run grpchttp3
+docker run envoy &
+docker run grpchttp3
