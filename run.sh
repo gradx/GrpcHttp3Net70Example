aa5c65ceb2f28f425f07063a9fd79191cb1eb1a0
gradx <gradx7@gmail.com>
2023-01-17 00:37:20 -0800

    testing envoy build, removing unused Dockerfile

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 
 #!/bin/bash
-sh SSL/certs.sh
+#sh SSL/certs.sh
 
 # DNS certificate
-#sh SSL/certs.sh int.mydomain.com
+sh SSL/certs.sh int.mydomain.com
 
 docker build -t grpchttp3 -f grpc.Dockerfile .
 docker build -t envoy -f envoy.Dockerfile .
